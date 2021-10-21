f99d6f5fa63bd3afe74ba4d2eeea3f43ee385d5c
evrimburcun <evrimbrcn@gmail.com>
2021-10-21 14:10:06 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,30 @@
+console.log("Merhaba kodlama.io")
+
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+dolarDun = "9.20"
+
+{
+  let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11 // bunu yapamıyoruz
+console.log(euroDun)
+
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+
+for (let i = 0;i<konutKredileri.length;i++){
+   console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
