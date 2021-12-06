9da3358dd0c637e4dfb3f0af06d8f7ddc58120f0
evrimburcun <evrimbrcn@gmail.com>
2021-12-06 14:03:51 +0300

    4.DERSNOTU

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -12,15 +13,28 @@ let user1 = new User(1,"Engin","Demiroğ","Ankara")
 let user2 = new User(2,"Baran","Gökçekli","Muğla")
 userService.add(user1)
 userService.add(user2)
-console.log(userService.list())
-console.log(userService.getById(2))
+// console.log(userService.list())
+// console.log(userService.getById(2))
 
 
 
 
 
+let customer = {id:1, firstName:" Engin"}
 
+//prototyping
+customer.lastName = "Demiroğ"
+console.log(customer.lastName)
 
+console.log("----------------------------------------")
+userService.load()
 
+let customerToAdd = new Customer(1,"Seda","Yılmaz","Ankara","fgdfgfhdsj")
+customerToAdd.type = "customer"
 
+userService.add(customerToAdd)
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
 
